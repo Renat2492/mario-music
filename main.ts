5b58072d9864d96011c6a4f656c467ce83227cb6
Renat2492 <125185019+Renat2492@users.noreply.github.com>
2023-02-12 14:02:40 +0400

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Double))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(233, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(147, music.beat(BeatFraction.Whole))
+    music.playTone(126, music.beat(BeatFraction.Whole))
+})
+music.setTempo(250)
+control.inBackground(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
